5196b6859cdd8ac62f04271101a1f35582496a69
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 01:34:13 +0530

    Update 31.2 Taking Element User Matrix.py

--- 31.2 Taking Element User Matrix.py
+++ 31.2 Taking Element User Matrix.py
@@ -21,23 +21,25 @@ for row in matrix:
 
 print()
 
-# Please enter any row numbers: 3
-# Please enter any column number: 3
+"""
+Please enter any row numbers: 3
+Please enter any column number: 3
 
-# Please enter  9  elements for matrix
-# A[1][1] = 1
-# A[1][2] = 2
-# A[1][3] = 3
+Please enter  9  elements for matrix
+A[1][1] = 1
+A[1][2] = 2
+A[1][3] = 3
 
-# A[2][1] = 4
-# A[2][2] = 5
-# A[2][3] = 6
+A[2][1] = 4
+A[2][2] = 5
+A[2][3] = 6
 
-# A[3][1] = 7
-# A[3][2] = 8
-# A[3][3] = 9
+A[3][1] = 7
+A[3][2] = 8
+A[3][3] = 9
 
 
-# A =      1 2 3 
-#          4 5 6 
-#          7 8 9 
+A =      1 2 3 
+         4 5 6 
+         7 8 9 
+"""
