f6896539faf78624e757f5306d3d93538ec5b341
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 18:26:15 +0530

    Create 01 Right-Angled Triangle Star Pattern.py

---
+++ 33 Patterns/01 Right-Angled Triangle Star Pattern.py
@@ -0,0 +1,21 @@
+n = int(input("Please enter any row number: "))
+
+for row in range(n+1):
+    print(row*"*")
+
+
+
+
+"""
+Please enter any row number: 7
+
+*
+**
+***
+****
+*****
+******
+*******
+
+
+"""
