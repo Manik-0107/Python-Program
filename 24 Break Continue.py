633c5d71606fec414d488f1349ad89a6d4ce5002
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 17:36:11 +0530

    Create 24 Break Continue.py

---
+++ 24 Break Continue.py
@@ -0,0 +1,36 @@
+i = 1
+while i <= 20:
+
+    print(i)
+    i += 1
+    if i ==7:
+        i += 1
+        continue
+    if i == 16:
+        break
+
+
+print("\nProgram is successfully executed...\n")
+
+
+
+
+"""
+1
+2
+3
+4
+5
+6
+8
+9
+10
+11
+12
+13
+14
+15
+
+Program is successfully executed...
+
+"""
