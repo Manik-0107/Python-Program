eccb646a761209e1b5d919392739812156186343
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 12:06:08 +0530

    Create 01 Print 1 to n.py

---
+++ 24 While Loop/01 Print 1 to n.py
@@ -0,0 +1,19 @@
+n = int(input("Please enter any number: "))
+
+i = 1
+while i <=n:
+    print(i, end=" ")
+    i += 1
+
+print("\nThe Program is successfully executed")
+
+
+
+
+
+"""
+Please enter any number: 7
+1 2 3 4 5 6 7 
+The program is successfully executed
+
+""
