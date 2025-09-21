9f27c5a4a56cbed1ff51c5dc0bab4592a4581a3c
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 18:34:40 +0530

    Create 04 Right-Angled Number Triangle Pattern.py

---
+++ 33 Patterns/04 Right-Angled Number Triangle Pattern.py
@@ -0,0 +1,22 @@
+n = int(input("Please enter any number of rows for right-angle pattern: "))
+
+for row in range(1, n+1):
+    for col in range(1, row+1):
+        print(col, end=" ")
+    print()
+
+
+
+
+
+"""
+Please enter any number of rows for right-angle pattern: 5
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+
+
+"""
