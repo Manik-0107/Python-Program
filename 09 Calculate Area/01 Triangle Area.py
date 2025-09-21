96e9575f931da95f30766634d195945310e7b7a5
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 10:03:58 +0530

    Create 01 Triangle Area.py

---
+++ 09 Calculate Area/01 Triangle Area.py
@@ -0,0 +1,16 @@
+# Calculate Triangle Area
+
+base = float(input("Please enter any triangle base: "))
+height = float(input("Please enter any triangle height: "))
+
+area = (1/2) * base * height
+
+print("Triangle Area is: ", area)
+
+"""
+
+Please enter any triangle base: 5
+Please enter any triangle height: 3
+Triangle Area is:  7.5
+
+"""
