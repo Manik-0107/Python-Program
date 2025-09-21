9354c8cfa9469a85112f45f22d6741b88b4b8de6
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 17:45:18 +0530

    Create 28 for loop.py

---
+++ 28 for loop.py
@@ -0,0 +1,39 @@
+
+num = [10, 20, 30, 40, 50]
+print(num)
+
+# iterate the list item using a while loop
+iterate = 0
+print("\nAfter Iterate: ")
+while iterate < len(num):
+    print(num[iterate])
+    iterate += 1
+
+
+# iteration the list item using for loop
+print("\n\nIterate list itean suing for loop")
+for i in num:
+    print(i)
+
+
+
+
+"""
+[10, 20, 30, 40, 50]
+
+After Iterate: 
+10
+20
+30
+40
+50
+
+
+Iterate list itean suing for loop
+10
+20
+30
+40
+50
+
+""
