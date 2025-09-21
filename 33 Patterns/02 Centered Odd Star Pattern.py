a56e7137a8fd0a51a179398f53a921de6a6606a6
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 18:30:26 +0530

    Create 02 Centered Odd Star Pattern.py

---
+++ 33 Patterns/02 Centered Odd Star Pattern.py
@@ -0,0 +1,21 @@
+# Pyramid Star Pattern (specifically, an odd-number pyramid or centred pyramid).
+
+n = int(input("Please enter any row number for Pyramid: "))
+for i in range(n+1):
+    print((2*i-1)*"*")
+
+
+
+
+"""
+Please enter any row number for Pyramid: 7
+
+*
+***
+*****
+*******
+*********
+***********
+*************
+
+"""
