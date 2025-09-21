8a307739cbe5cef4955326cfaa653ef7a59beb7d
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 18:45:25 +0530

    Create 05 Right-Angled Binary Number Triangle Pattern.py

---
+++ 33 Patterns/05 Right-Angled Binary Number Triangle Pattern.py
@@ -0,0 +1,25 @@
+n = int(input("Please enter any row number: "))
+
+# for row in range(1, n+1):
+#     for col in range(1, row+1):
+#         print(col%2, end=" ")
+#     print()
+
+for row in range(1, n+1):
+    for col in range(1, row+1):
+        print(col%2, end=" ")
+    print()
+
+
+
+
+
+"""
+Please enter any row number: 5
+1 
+1 0 
+1 0 1 
+1 0 1 0 
+1 0 1 0 1 
+
+"""
