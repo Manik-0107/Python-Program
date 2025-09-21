40c032682f598f9e374a8abacf69600930edad93
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 10:22:23 +0530

    Create 05 Parallelogram.py

---
+++ 09 Calculate Area/05 Parallelogram.py
@@ -0,0 +1,18 @@
+# Calculate Parallelogram area
+
+base = float(input("\nPlease enter base for the Parallelogram: "))
+vertical_height = float(input("Please enter vertical_height for the Parallelogram: "))
+
+area = base * vertical_height
+
+print("Parallelogram area is: ", area),"\n"
+
+
+
+
+"""
+Please enter base for the Parallelogram: 5
+Please enter vertical_height for the Parallelogram: 3
+Parallelogram area is:  15.0
+
+"""
