ae36da7e905d9dd3d92e97beca68679c63f65801
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:49:05 +0530

    Create 15 if else PassFail.py

---
+++ 15 if else PassFail.py
@@ -0,0 +1,20 @@
+# Pass & Fail
+
+mark = int(input("Please enter your mark: "))
+
+if mark >= 33:
+    print("Your are pass ...")
+    print("Congrasulation")
+
+else:
+    print("You are fail !!!")
+    print("Try again later...")
+
+
+
+"""
+Please enter your mark: 75
+Your are pass ...
+Congrasulation
+
+"""
