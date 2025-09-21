d6a37dff8193af44f03eb68f8f76d0dc9b4bf914
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 19:05:13 +0530

    Create 13 Solid Square Pattern.py

---
+++ 33 Patterns/13 Solid Square Pattern.py
@@ -0,0 +1,21 @@
+n = int(input("Please enter any number of rows: "))
+print()
+for row in range(1, n+1):
+    for col in range(1, n+1):
+        print("*", end=" ")
+    print()
+
+
+
+
+"""
+
+Please enter any number of rows: 5
+
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+"""
