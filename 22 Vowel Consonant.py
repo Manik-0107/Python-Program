f6f19785b9d36930f9b46b6c980506c1e482d04c
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:58:27 +0530

    Create 22 Vowel Consonant.py

---
+++ 22 Vowel Consonant.py
@@ -0,0 +1,24 @@
+
+# Vowel and Consonant
+
+ch = input("Please enter any letter: ")
+
+if ch=='a'or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U':
+    print(ch, " is the Vowel")
+
+else:
+    print(ch," is the consonant")
+
+
+
+
+
+
+"""
+Please enter any letter: s
+s  is the consonant
+
+Please enter any letter: e
+e  is the Vowel
+
+"""
