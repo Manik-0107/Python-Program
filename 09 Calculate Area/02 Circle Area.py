13b74461b29498838a3257108a0315df5e083ccf
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 10:05:08 +0530

    Create 02 Circle Area.py

---
+++ 09 Calculate Area/02 Circle Area.py
@@ -0,0 +1,19 @@
+# Calculate Circle Area
+
+radius = float(input("Please enter any radius for circle: "))
+
+area = (21/7) * radius * radius
+
+print("\nCircle Area is: ", area)
+
+
+
+
+
+
+"""
+Please enter any radius for circle: 7
+
+Circle Area is:  147.0
+
+"""
