34f5da0b524dc52ce887e68c3f3407c6d635122a
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 09:59:58 +0530

    Create 08 Type  Casting.py

---
+++ 08 Type  Casting.py
@@ -0,0 +1,28 @@
+n1 = int(input("Please enter any first number: "))
+n2 = int(input("Please enter any second number: "))
+
+# n1 = input("Please enter any second number: ")
+# n2 = input("Please enter any second number: ")
+# result = int(n1) + int(n2)
+# You can convert float, double, etc..........
+
+print("\nAddition is: ", n1+n2)
+print("Subtraction is: ", n1-n2)
+print("Multiplication is: ", n1 * n2)
+print("Division is: ", n1 / n2)
+print("Floor is: ", n1 // n2)
+print("Exponentiation is: ", n1 ** n2)
+
+"""
+
+Please enter any first number: 45
+Please enter any second number: 12
+
+Addition is:  57
+Subtraction is:  33
+Multiplication is:  540
+Division is:  3.75
+Floor is:  3
+Exponentiation is:  68952523554931640625
+
+""
