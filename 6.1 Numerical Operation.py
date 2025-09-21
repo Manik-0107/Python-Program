1f675beb59ec53775a62cf0b654c95fe2b2c7cd7
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 09:14:35 +0530

    Create 6.1 Numerical Operation.py

---
+++ 6.1 Numerical Operation.py
@@ -0,0 +1,24 @@
+# +, -, *, /, %, //(Floor), **(Exponentiation)
+
+n1 = 11
+n2 = 4
+
+print("\nAddition: ",n1 + n2)
+print("Subtraction: ",n1 - n2)
+print("Multiplication: ",n1 * n2)
+print("Divided: ",n1 / n2)
+print("Modulas: ",n1 % n2)
+print("Floor value: ",n1 // n2)
+print("Exponential: ",n1 ** n2)
+print()
+
+"""
+Addition:  15
+Subtraction:  7
+Multiplication:  44
+Divided:  2.75
+Modules:  3
+Floor value:  2
+Exponential:  14641
+
+"""
