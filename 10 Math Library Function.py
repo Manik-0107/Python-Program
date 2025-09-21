ac40a6f33c4cab42c7f001fa70510ea01b8e9fb8
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:40:02 +0530

    Create 10 Math Library Function.py

---
+++ 10 Math Library Function.py
@@ -0,0 +1,23 @@
+from math import *
+
+print("\nMaximum: ", max(10, 20))
+print("Minimum: ", min(10, 20))
+print("Absolute: ", abs(-7))
+print("Square Root: ", sqrt(25))
+print("Power: ", pow(3, 2))
+print("Round value: ", round(7.5))
+print("Floor value: ", floor(7.88))
+print("celling value: ", ceil(7.88))
+
+
+"""
+Maximum:  20
+Minimum:  10
+Absolute:  7
+Square Root:  5.0
+Power:  9.0
+Round value:  8
+Floor value:  7
+celling value:  8
+
+"""
