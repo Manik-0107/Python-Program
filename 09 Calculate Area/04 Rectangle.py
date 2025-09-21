83fbea8e9b0e16ccf88e218c82cf35ff7adc648e
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 10:18:24 +0530

    Create 04 Rectangle.py

---
+++ 09 Calculate Area/04 Rectangle.py
@@ -0,0 +1,18 @@
+# Calculate Rectangle area
+
+width = float(input("\nPlease enter any width for rectangle: "))
+height = float(input("Please enter any height for rectangle: "))
+
+area = width * height
+
+print("The Rectangle area is: ", area,"\n")
+
+
+
+
+"""
+Please enter any width for rectangle: 7
+Please enter any height for rectangle: 5
+The Rectangle area is:  35.0 
+
+"""
