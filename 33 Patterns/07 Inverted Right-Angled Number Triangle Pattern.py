a064df8ed732488955828b634fb1d16e2dacf969
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 18:48:26 +0530

    Create 07 Inverted Right-Angled Number Triangle Pattern.py

---
+++ 33 Patterns/07 Inverted Right-Angled Number Triangle Pattern.py
@@ -0,0 +1,20 @@
+n = int(input("Please enter any number of rows: "))
+
+for row in range(n, 0, -1):
+    for col in range(1, row+1):
+        print(col, end=" ")
+    print()
+
+
+
+"""
+
+Please enter any number of rows: 5
+
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+
+"""
