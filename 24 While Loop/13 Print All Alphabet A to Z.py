338502f6cc3e53515a248221a283eedbd56a5d2b
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 14:34:21 +0530

    Create 13 Print All Alphabet A to Z.py

---
+++ 24 While Loop/13 Print All Alphabet A to Z.py
@@ -0,0 +1,13 @@
+# Print all alphabets from A to Z using a while loop.
+
+i = 65
+while i <= 90:
+    print(chr(i), end=" ")
+    i += 1
+
+
+
+"""
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
+
+"""
