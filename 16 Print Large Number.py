ff60ad3f884cbdd8ff0d40ee3e288d224d6ab9d3
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:50:13 +0530

    Create 16 Print Large Number.py

---
+++ 16 Print Large Number.py
@@ -0,0 +1,20 @@
+# Largest Number between 2 numbers
+
+n1 = int(input("Please enter first number: "))
+n2 = int(input("Please enter second number: "))
+
+if n1 > n2:
+    print(f"The largest number is: {n1}")
+
+else:
+    print(f"The largest number is: {n2}")
+
+
+
+
+"""
+Please enter first number: 10
+Please enter second number: 99
+The largest number is: 99
+
+"""
