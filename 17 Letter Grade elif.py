c29025e0085fd7927b53a7056f23f81fbed6b924
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:51:28 +0530

    Create 17 Letter Grade elif.py

---
+++ 17 Letter Grade elif.py
@@ -0,0 +1,25 @@
+# Letter Grade
+
+mark = int(input("Please enter your mark: "))
+
+if mark >= 80:
+    print("Grade: A+")
+elif mark >= 70:
+    print("Grade: A")
+elif mark >= 60:
+    print("Grade: -A")
+elif mark >= 50:
+    print("Grade: B")
+elif mark >= 40:
+    print("Grade: C")
+elif mark >= 330:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+
+"""
+Please enter your mark: 77
+Grade: A
+
+"""
