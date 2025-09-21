e8e52461bd2fb55f013d9dd4727162b161d81656
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:55:03 +0530

    Create 20 Ternary Operator.py

---
+++ 20 Ternary Operator.py
@@ -0,0 +1,16 @@
+# How to use Ternary Operator
+
+n1 = int(input("Please enter any first number: "))
+n2 = int(input("Please enter any second number: "))
+
+max = n1 if n1 > n2 else n2
+
+print("Largest number is: ", max)
+
+
+"""
+Please enter any first number: 10
+Please enter any second number: 20
+Largest number is:  20
+
+"""
