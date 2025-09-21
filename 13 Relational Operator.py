6aff1746b6bd9a2a0b452c100f7cbe570cbebd9c
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:44:44 +0530

    Create 13 Relational Operator.py

---
+++ 13 Relational Operator.py
@@ -0,0 +1,27 @@
+""" 
+ Relational Operator: >, <, >=, <=, ==, !=
+ Boolean Value: True & False
+ Because when you use a Relational Operator, the output always Boolean value
+"""
+
+print("30 > 20 : ", 30 > 20)
+print("30 < 20 : ", 30 < 20)
+print("30 >= 20 : ", 30 >= 20)
+print("30 <= 20 : ", 30 <= 20)
+print("30 != 20 : ", 30 != 20)
+print("30 == 20 : ", 30 == 20)
+print("Manik == Manik : ", "Manik" == "Manik")
+
+
+
+
+"""
+30 > 20 :  True
+30 < 20 :  False
+30 >= 20 :  True
+30 <= 20 :  False
+30 != 20 :  True
+30 == 20 :  False
+Manik == Manik :  True
+
+""
