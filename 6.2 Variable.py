52646449a59834712459db5fe0495cfb1cf1e2e5
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 09:17:35 +0530

    Create 6.2 Variable.py

---
+++ 6.2 Variable.py
@@ -0,0 +1,19 @@
+name = "Manik Mondal"
+age = 18
+cgpa = 7.21
+
+print("Our new student name is: "+name)
+print(name+" Lives in Karad")
+print("He is currently", age, "years old")
+print(name+" has scored", cgpa, "in his secondary exam")
+
+print("\nManik " + "Mondal " + "Surya")
+
+"""
+Our new student name is: Manik Mondal
+Manik Mondal Lives in Karad
+He is currently 18 years old
+Manik Mondal has scored 7.21 in his secondary exam
+
+Manik Mondal Surya
+"""
