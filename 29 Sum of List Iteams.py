1316342dc8dbe7ebf2fcf199458f778d8a5a8af7
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 17:47:36 +0530

    Create 29 Sum of List Iteams.py

---
+++ 29 Sum of List Iteams.py
@@ -0,0 +1,16 @@
+# Sum of list all item using for loop
+
+num = [10, 20, 30, 40, 50]
+
+sum = 0
+for i in num:
+    sum += i
+print("The Sum of all list items is: ", sum)
+
+
+
+
+"""
+The sum of all list items is:  150
+
+"""
