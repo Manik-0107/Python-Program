24143ffdfb02c41bcee024f4019b50a2e84b4c87
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 09:36:29 +0530

    Create 07 Taking Input Form User.py

---
+++ 07 Taking Input Form User.py
@@ -0,0 +1,24 @@
+# Taking input from user
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+cgpa = input("Please enter your CGPA: ")
+
+print("\nStudent Information")
+print("----------------------------")
+print("Name: "+name)
+print("Age: "+age)
+print("CGPA: "+cgpa)
+
+"""
+Please enter your name: Manik Mondal Surya
+Please enter your age: 18
+Please enter your CGPA: 10
+
+Student Information
+----------------------------
+Name: Manik Mondal Surya
+Age: 18
+CGPA: 10
+
+"""
