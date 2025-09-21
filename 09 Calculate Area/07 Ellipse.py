fd91a58487176fa4bd8ee137e11df8dd33148a03
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 10:33:32 +0530

    Create 07 Ellipse.py

---
+++ 09 Calculate Area/07 Ellipse.py
@@ -0,0 +1,18 @@
+# Calculate Ellipse area
+
+a = float(input("\nPlease enter any semi-major axis: "))
+b = float(input("Please enter any semi-minor axis: "))
+
+area = (22/7) * a * b
+
+print("The Ellipse area is: ", area,"\n")
+
+
+
+
+"""
+Please enter any semi-major axis: 7
+Please enter any semi-minor axis: 3
+The Ellipse area is:  66.0 
+
+"""
