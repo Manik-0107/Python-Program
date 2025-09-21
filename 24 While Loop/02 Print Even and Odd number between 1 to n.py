6b509e40c743e0c0c442df4759694aefc79effe0
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 12:08:03 +0530

    Create 02 Print Even and Odd number between 1 to n.py

---
+++ 24 While Loop/02 Print Even and Odd number between 1 to n.py
@@ -0,0 +1,28 @@
+n = int(input("Please enter any last number: "))
+
+i = 1
+print("Odd numbers are: ")
+while i <= n:
+    print(i, end=" ")
+    i += 2
+
+print("\nEven numbers are: ")
+i = 2
+while i <= n:
+    print(i, end=" ")
+    i += 2
+
+print("\n\nProgram is successfully executed")
+
+
+
+"""
+Please enter any last number: 27
+Odd numbers are: 
+1 3 5 7 9 11 13 15 17 19 21 23 25 27 
+Even numbers are: 
+2 4 6 8 10 12 14 16 18 20 22 24 26 
+
+Program is successfully executed
+
+"""
