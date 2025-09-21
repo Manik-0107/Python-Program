52b8eaadd0a80d5c52387276f817ef8b052b8cd4
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 17:43:33 +0530

    Create 27 Range Function.py

---
+++ 27 Range Function.py
@@ -0,0 +1,22 @@
+# How to use range() function in python
+
+num = list(range(12))
+print(num)
+print(num[3])
+
+num1 = list(range(5, 11))
+print(num1)
+
+num2 = list(range(2, 20, 2))
+print(num2)
+
+
+
+
+"""
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+3
+[5, 6, 7, 8, 9, 10]
+[2, 4, 6, 8, 10, 12, 14, 16, 18]
+
+"""
