0c38278333e2d09616b1866b8b4c9fa076df0c31
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:47:44 +0530

    Create 14 Check Even Odd Number.py

---
+++ 14 Check Even Odd Number.py
@@ -0,0 +1,20 @@
+# Findout Even & Odd number
+
+num = int(input("Please enter any integer number: "))
+
+if num % 2 == 0:
+    print(f"{num} is the an Even number")
+
+else:
+    print(f"{num} is the an Odd number")
+
+
+
+"""
+Please enter any integer number: 7
+7 is an Odd number
+
+Please enter any integer number: 8
+8 is an Even number
+
+"""
