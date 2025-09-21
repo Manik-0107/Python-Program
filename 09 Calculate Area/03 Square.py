0c953a5197217a282ef8a17a8e1a5a944e49b9b9
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 10:14:32 +0530

    Create 03 Square.py

---
+++ 09 Calculate Area/03 Square.py
@@ -0,0 +1,13 @@
+length = float(input("Please enter any length of side of the Square: "))
+
+area = length*length
+
+print("Area of the Square: ", area)
+
+
+
+"""
+Please enter any length of side of the Square: 7
+Area of the Square:  49.0
+
+"""
