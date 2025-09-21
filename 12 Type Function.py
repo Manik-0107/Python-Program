696930f4fdfa26c12cb433e5f43c2cd55978a614
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:43:04 +0530

    Create 12 Type Function.py

---
+++ 12 Type Function.py
@@ -0,0 +1,22 @@
+num = 10
+print(type(num))
+
+print(type(30))
+print(type(2.3))
+print(type("Manik"))
+
+print(type(True))
+print(type(False))
+
+
+
+
+"""
+<class 'int'>
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'bool'>
+<class 'bool'>
+
+"""
