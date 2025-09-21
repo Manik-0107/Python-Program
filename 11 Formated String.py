c0a4075403313927a0c123d0480f333ff3889749
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 11:41:58 +0530

    Create 11 Formated String.py

---
+++ 11 Formated String.py
@@ -0,0 +1,22 @@
+# Formatted String
+
+num1 = 20
+num2 = 30
+
+result = num1 + num2
+
+print(f"{num1} + {num2} = {result}")
+
+# print same line, not use next line
+print("\n\nManik ", end="")
+print("Mondal")
+
+
+
+"""
+20 + 30 = 50
+
+
+Manik Mondal
+
+"""
