fab8a9dfeea92a6c409d63afe23123c069bf1d59
Manik-0107 <manikmondal0107@gmail.com>
2025-09-21 10:29:13 +0530

    Create 06 TrapeziumUK TrapezoidUSA.py

---
+++ 09 Calculate Area/06 TrapeziumUK TrapezoidUSA.py
@@ -0,0 +1,20 @@
+# Calculate Trapezoid(US) / Trapezium (UK) area
+
+a = float(input("\nPlease enter top base size: "))
+b = float(input("Please enter bottom base size: "))
+h = float(input("Please enter vertical height size: "))
+
+area = 0.5*(a + b) * h
+
+print("The Trapezoid area is: ", area,"\n")
+
+
+
+
+"""
+Please enter top base size: 3
+Please enter bottom base size: 5
+Please enter vertical height size: 2
+The Trapezoid area is:  8.0 
+
+"""
