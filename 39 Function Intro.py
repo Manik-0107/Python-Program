aab95fa5bc67c048ba3262359ba3e3f290edf545
Manik-0107 <manikmondal0107@gmail.com>
2025-09-23 20:35:09 +0530

    Create 39 Function Intro.py

---
+++ 39 Function Intro.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    sum = x + y
+    print("Addition: ", sum)
+
+def sub(a, b):
+    subt = a - b
+    print("Subtraction: ", subt)
+
+def message():
+    print("----- Never Give Up ----")
+
+add(10, 20)
+sub(50, 10)
+message()
+
+
+
+"""
+
+Addition:  30
+Subtraction:  40
+----- Never Give Up ----
+
+"""
