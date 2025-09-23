d6dddd380290d15e6876abdc3c224381569e6b84
Manik-0107 <manikmondal0107@gmail.com>
2025-09-23 21:30:45 +0530

    Update 41 Xargs(X - arguments).py

--- 41 Xargs(X - arguments).py
+++ 41 Xargs(X - arguments).py
@@ -8,6 +8,14 @@ student(101, "Manik")
 student(102, "Rahul", 25)
 
 
+def add(*details):
+    sum = 0
+    for num in details:
+        sum += num
+    print("Addition is: ", sum)
+add(10, 20)
+
+
 
 """
 
@@ -15,5 +23,7 @@ student(102, "Rahul", 25)
 101
 (101, 'Rahul', 25)
 101
+
+Addition is: 30
 
 """
