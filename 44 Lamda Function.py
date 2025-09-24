21495bf664fe97df5e1fa0c842d3090b72a3fe5c
Manik-0107 <manikmondal0107@gmail.com>
2025-09-24 16:23:21 +0530

    Update 44 Lamda Function.py

--- 44 Lamda Function.py
+++ 44 Lamda Function.py
@@ -3,6 +3,8 @@
 --> A Lambda function is also called an Anonymous function, because it has no name.
 --> Not as powerful as the name function
 --> It can work with a single expression / single line of code
+--> Syntax:
+            lambda parameter: expression
 
 """
 
